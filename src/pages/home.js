e912671c134f77db9f9b582fc5858ffe9008b6d4
EdsonHTJ <edsonsobralense@gmail.com>
2022-03-23 22:16:01 -0300

    update menu

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,9 +4,9 @@ function Home() {
       <div className="App">
           <div className="containerHome">
              <h1>Algoritimos Fundamentais</h1>
-             <a href="fib" className="btn">Fibonacci</a>
-             <a href="sum" className="btn">Somatorio</a>
              <a href="prime" className="btn">Checagem de primo</a>
+             <a href="sum" className="btn">Somatorio</a>
+             <a href="fib" className="btn">Fibonacci</a>
              <a href="mdc" className="btn">MDC</a>
              <a href="sort" className="btn">Ordenação</a>
              <a href="count" className="btn">Contagem</a>
